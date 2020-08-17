393cdd3f9f7320a776fafc89a95d83994968cb6d
cadtay <callum.taylor@carfinance247.co.uk>
2020-08-17 11:00:53 +0100

    Added controller

---
+++ HackDay/HackDay/Controllers/FloodController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackDay.Controllers
+{
+    public class FloodController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
