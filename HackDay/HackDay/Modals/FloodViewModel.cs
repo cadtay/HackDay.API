a1458b724cb11b3ab5a2e658606f9a89f3c520f9
cadtay <callum.taylor@carfinance247.co.uk>
2020-08-17 13:16:50 +0100

    Added flood endpoint, changed default start URL, added getting flood data

---
+++ HackDay/HackDay/Modals/FloodViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackDay.Modals
+{
+    public class FloodViewModel
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Distance { get; set; }
+    }
+}
--- HackDay/HackDay/Startup.cs
+++ HackDay/HackDay/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HackDay.Repository;
+using HackDay.Repository.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -27,6 +29,8 @@ namespace HackDay
             services.AddHttpClient();
 
             services.AddControllers();
+
+            services.AddScoped<IFloodRepository, FloodRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
