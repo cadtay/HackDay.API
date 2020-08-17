2fc79df06364381b0c0cbe47a953d39c7745b920
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-08-17 11:24:49 +0100

    Add StreetCrimesController

---
+++ HackDay/HackDay/Controllers/StreetCrimesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackDay.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StreetCrimesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
