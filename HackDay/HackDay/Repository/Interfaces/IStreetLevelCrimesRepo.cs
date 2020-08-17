b4b7d6af4a7eeeb0fd6a3aa2ad0cd661062df602
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-08-17 11:29:34 +0100

    Add Model & Interface

---
+++ HackDay/HackDay/Repository/Interfaces/IStreetLevelCrimesRepo.cs
@@ -0,0 +1,17 @@
+﻿using HackDay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackDay.Repository.Interfaces
+{
+    public interface IStreetLevelCrimesRepo
+    {
+        Task<StreetLevelCrimes[]> GetAllStreetLevelCrimesByLocation();
+        Task<StreetLevelCrimes[]> GetAllStreetLevelCrimesByLocationAndTime(string date);
+        Task<StreetLevelCrimes[]> GetAllStreetLevelCrimesByLocationAndCategory(string category);
+        Task<StreetLevelCrimes[]> GetAllStreetLevelCrimesByLocationAndCategoryAndTime(string category, string date);
+        Task<StreetLevelCrimes> GetStreetLevelCrimeById(int id);
+    }
+}
